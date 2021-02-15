81cad129de976d365cc8a5ca025f66143487f761
jjfenner93 <joshfenner93@icloud.com>
2021-02-15 15:45:09 +0000

    hcf implemented

--- hcf.c
+++ hcf.c
@@ -29,11 +29,17 @@
     // unless you first check both numbers to see which is highest.
     // if false it can check in the other condition?
 
+
     for i <= y
     if y MOD i == 0
     secondValue = i
     endif
     endfor
+
+    if a number goes into it without a remainder then
+    it should be recorded that it happened but at the same time as the other number
+    get the last match, could be based on a condition checking
+    if the loop is still going,
 
     
 */
@@ -41,23 +47,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 unsigned int hcf(unsigned int x, unsigned int y);
 
 int main(int argc, char *argv[])
 {
-    // hcf(12, 18); // That should return 6.
+    unsigned int hcfNo = hcf(12, 18); // That should return 6.
+    printf("%d", hcfNo);
 
     return 0;
 }
 
 unsigned int hcf(unsigned int x, unsigned int y)
 {
-    for (int i = 1; i < x; i++)
-    {
-        if (i % x == 0)
+    unsigned int firstValue = 0;
+    unsigned int secondValue = 0;
+    unsigned int checker = 0;
+    unsigned int lastValue = 0;
 
+    // This code is used so that it will never
+    // use a number lower (goes from the highest to compare).
+    if (x > y) {
+        checker = x;
+    } else {
+        checker = y;
     }
+
+    for (int i = 1; i < checker; i++)
+    {
+        if (x % i == 0)
+        {
+            firstValue = i;
+        }
+
+        if (y % i == 0)
+        {
+            secondValue = i;
+        }
+
+        if (firstValue == secondValue)
+        {
+            lastValue = secondValue;
+        }
+    }
+    return lastValue;
 }
 
 void function2(void)

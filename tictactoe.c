385db34fe5248ebacc0103ad1327cdd383f7bbdd
jjfenner93 <joshfenner93@icloud.com>
2021-02-18 15:49:31 +0000

    prepared plan for tictactoe

---
+++ tictactoe.c
@@ -0,0 +1,12 @@
+/*
+    Plan: Write a program that plays tic tac toc.
+    Game is played on a 3x3 grid, played by two players (take turns).
+    You should create an array to represent the board.
+    Can be of type char and consist of 10 elements (do not use zero).
+    Each element represents a co-ordinate on the board that the user can select.
+    Some functions that you can probably create:
+    checkForWin - checks to see if a player has won or the game is a draw.
+    drawboard - redraws the board for each player turn.
+    markBoard - sets the char array with a selection 
+    and check for an invalid selection.
+*/
